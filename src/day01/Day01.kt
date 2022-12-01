ce4584661f14a5c0658a9146c89af258984df6d0
epopov <mr.demix@yandex.ru>
2022-12-01 19:07:42 +0300

    Day 1 both parts

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,17 +1,42 @@
+package day01
+
+import readInput
+import separateInputByEmptyLine
+
 fun main() {
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputByEmptyLine = separateInputByEmptyLine(input)
+        val maxValue = inputByEmptyLine
+            .stream()
+            .map {
+                it.stream().mapToInt(String::toInt).sum()
+            }
+            .toList()
+            .max()
+        return maxValue
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputByEmptyLine = separateInputByEmptyLine(input)
+        val sumTopThree = inputByEmptyLine
+            .asSequence()
+            .map {
+                it.stream().mapToInt(String::toInt).sum()
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
+        return sumTopThree
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day01/Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -14,3 +14,20 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+fun separateInputByEmptyLine(input: List<String>): List<List<String>> {
+    val entitiesList = ArrayList<List<String>>()
+    var entity = ArrayList<String>()
+    val iterator = input.listIterator()
+    while (iterator.hasNext()) {
+        val line = iterator.next()
+        if (line.isNotEmpty()) {
+            entity.add(line)
+        } else if (line.isEmpty()) {
+            entitiesList.add(entity)
+            entity = ArrayList()
+        }
+    }
+    entitiesList.add(entity)
+    return entitiesList
+}
